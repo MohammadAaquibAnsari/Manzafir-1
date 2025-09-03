621f03f68479598dc5b159dde9711c66000cb49a
AdiNandan <adimanish29@gmail.com>
2025-09-04 00:51:41 +0530

    fix(ml): Update CORS to allow production frontend

--- ml/recomm.py
+++ ml/recomm.py
@@ -41,7 +41,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Your frontend's URL
+    allow_origins=[
+        "https://manzafir-wl2h.onrender.com",
+        "http://localhost:3000"],  # Your frontend's URL
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
